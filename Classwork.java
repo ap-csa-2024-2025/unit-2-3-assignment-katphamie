50ebc217f3b387e971643fba82f6b3ae0245d64e
katphamie <katnphmie@gmail.com>
2024-09-27 10:51:39 -0700

    meow

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,41 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN("dictionary", 3));
   }
 
   // write solutions to problems below
+  public static String firstLastN(String word, int n)
+  {
+    // word = dictionary
+    // n = 3
+    //dicary
+    String output = new String();
 
+    // Getting the first n letters
+    String firstN = word.substring(0, n); //first n letters; substring from 0 to n-1
+    
+    // Getting the last n letters
+    int indexOfNToLast = word.length() - n; // index of the nth to last letter
+    String lastN = word.substring(indexOfNToLast);
+    
+    output = firstN + lastN;
+    return output;
+  }
+
+
+
+
+//DelEnd
+public static void main(String[] args)
+{
+  System.out.println(delEnd("library", 5))
+}
+
+public static String delEnd(String word, int n)
+{
+  String output = new String();
+
+  String first
+}
 }
